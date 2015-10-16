6217b3d4928dccd848191bb7f435dd128856bb9f
eric <baixiangcpp@gmail.com>
2015-10-16 19:23:21 +0800

    main

--- main.cpp
+++ main.cpp
@@ -4,19 +4,11 @@
 
 int main()
 {
-<<<<<<< HEAD
     if(!Daemonize::beDaemon())
     {
         MessageLog* logger = MessageLog::getLogger();
         logger->errLog("This process cannnot be daemon.\n");
     }
-=======
-    /*if(!Daemonize::beDaemon())
-    {
-        MessageLog* logger = MessageLog::getLogger();
-        logger->errLog("This process cannnot be daemon.\n");
-    }*/
->>>>>>> e972e68a5cddb4f8815309c824fa6977b59f5033
     WebServer wser;
     wser.run();
 }
