16d1760a3f44cd8f749ab96d3fc28b6aab90bbbf
eric <baixiangcpp@gmail.com>
2015-10-19 20:09:44 +0800

    add comment

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -32,11 +32,11 @@ void* ThreadPool::thread_routine(void *arg)
     {
         pthread_mutex_lock(&pt->m_mutex);
         /*******************************************************
-         * if there are tasks in std::list 
-         * get one immediately,instead of pthread_cond_wait().
-         * OtherWise we may block here(wait for condition var),
-         * when tasks are in std::list
-         * ****************************************************/
+        * if there are tasks in std::list 
+        * get one immediately,instead of pthread_cond_wait().
+        * OtherWise we may block here(wait for condition var),
+        * when tasks are in std::list
+        *******************************************************/
         while(!pt->m_ready)
         {
             pthread_cond_wait(&pt->m_cond, &pt->m_mutex);
